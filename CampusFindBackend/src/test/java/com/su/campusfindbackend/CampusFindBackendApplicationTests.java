8e57e0a49ab203fd5be9d856efa97603078bd5ea
Vedavyaas M R <vedavyaasmr@Vedavyaass-MacBook-Air.local>
2026-09-19 12:43:22 +0530

    Project skeleton

---
+++ CampusFindBackend/src/test/java/com/su/campusfindbackend/CampusFindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su.campusfindbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusFindBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
